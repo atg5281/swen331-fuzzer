322078ef6919ded7859ac73358bbe773d6382e34
U-SNARES-PENGUIN\James <jcd7042@rit.edu>
2016-02-16 20:28:54 -0500

    Partially implemented fuzz discover command. Finds links recursively on pages.

--- fuzzer/fuzz.py
+++ fuzzer/fuzz.py
@@ -1,5 +1,8 @@
 import getopt
+import requests
+from bs4 import BeautifulSoup
 import sys
+from urllib.parse import urljoin, urlparse
 
 helpStr = ("COMMANDS:\n"
            "\tdiscover  Output a comprehensive, human-readable list of all discovered inputs to the system."
@@ -33,11 +36,64 @@ def main(argv):
             raise getopt.GetoptError("")
         else:
             command = argv[0]
-            arguments = argv[1:] if len(argv) > 1 else []
-        opts, args = getopt.getopt(arguments, "", ['custom-auth-string=', 'common-words=', 'vectors=', 'random=',
-                                                   'slow='])
+            arguments = argv[2:] if len(argv) > 1 else []
+            opts, args = getopt.getopt(arguments, "", ['custom-auth-string=', 'common-words=', 'vectors=', 'random=',
+                                                       'slow='])
+            initial_url = argv[1]
+            common_words = []
+
+            for opt, arg in opts:
+                if opt == 'custom-auth-string':
+                    initial_url = authenticate(argv[1])
+                elif opt == 'common-words':
+                    for line in open(arg):
+                        common_words.append(line)
+            
+                
+            if command == 'discover':
+                discover(initial_url, common_words)
+        
+            
     except getopt.GetoptError:
         print(helpStr)
+
+def authenticate(url):
+    pass
+
+def discover(url, common_words):
+    links = discover_links(url, urlparse(url).netloc)
+    print(links)
+
+    for link in links:
+        discover_print_inputs(link)
+
+def discover_links(initial_url, site, discovered_urls=set()):
+    print('discover_links: ' + 'initial_url = ' + initial_url);
+    response = requests.get(initial_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # import pdb; pdb.set_trace()
+    if response.status_code == 200 and not response.url in discovered_urls:
+        found_urls = {response.url}
+        
+        for link in soup.find_all('a'):
+            url = link.get('href')
+            url_site = urlparse(url).netloc
+            if not url == None and (url_site == site or url_site == ''):
+                found_urls.add(urljoin(response.url, url))
+            
+        for url in found_urls:
+            found_urls = found_urls.union(discover_links(url, site, discovered_urls.union({initial_url})))
+        
+        return found_urls
+    else:
+        return set()
+
+def discover_guess_links(base_url, common_words):
+    return {}
+
+def discover_print_inputs(link):
+    pass
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
